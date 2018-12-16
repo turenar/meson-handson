d909377772e45e84b8ebfee0de4ec87b2cc8e923
Turenar <illyasviel@turenar.xyz>
2018-12-17 03:17:36 +0900

    lua 5.3 doesn't have lua_open()

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <lua.hpp>
 
 int main() {
-    lua_State* state = lua_open();
+    lua_State* state;
+#if LUA_VERSION_NUM >= 501
+    state = luaL_newstate();
+#else
+    state = lua_open();
+#endif
     luaL_openlibs(state);
     luaL_dostring(state, "print \"Hello world!\"");
     lua_close(state);
